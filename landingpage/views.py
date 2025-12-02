3463504479ac17fbf322cf1373d8d2d4fc9d6f58
bics <bicsi94@gmail.com>
2025-12-02 20:55:31 +0000

    CH-Adding register page

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, "register.html", { "form": form})
+
