786acbf847129f128375ac5a0216605a9a6bd10d
bics <bicsi94@gmail.com>
2025-12-02 22:15:20 +0000

    CH-Styling register form

--- landingpage/views.py
+++ landingpage/views.py
@@ -6,6 +6,18 @@ def home(request):
     return render(request, "index.html")
 
 def register(request):
-    form = UserCreationForm()
+    # Snippet taken from ChatGPT
+    placeholders = {
+        "username": "Username",
+        "password1": "Password",
+        "password2": "Confirm Password",
+    }
+
+    form = UserCreationForm()    
+    # Snippet taken from ChatGPT
+    for name, field in form.fields.items():
+        field.widget.attrs.setdefault("class", "")
+        field.widget.attrs["class"] += " form-control-lg w-100"
+        field.widget.attrs["placeholder"] = placeholders.get(name, "")
     return render(request, "register.html", { "form": form})
 
