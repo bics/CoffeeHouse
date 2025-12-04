764075d2dfe52f0d4f9dbf4f2c4f562dd03da6df
bics <bicsi94@gmail.com>
2025-12-04 04:40:42 +0000

    CH-Moving registration into members app

--- landingpage/views.py
+++ landingpage/views.py
@@ -1,25 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from django.contrib.auth.forms import UserCreationForm
-from .forms import RegisterForm
 from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
-
-# view created following tutorial made by Codemy.com https://www.youtube.com/watch?v=HdrOcreAXKk
-def register(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            #login(request, user)
-            #message
-            return redirect('home')
-    else:
-        form = RegisterForm()
-    return render(request, "register.html", { "form": form})
--- members/views.py
+++ members/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 # Create your views here.
+# view created following tutorial made by Codemy.com https://www.youtube.com/watch?v=HdrOcreAXKk
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            #login(request, user)
+            #message
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, "authentication/register.html", { "form": form})
