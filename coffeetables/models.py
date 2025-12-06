0a022cce7d1220e9bbde8cd45bc03eb0ba153a1d
bics <bicsi94@gmail.com>
2025-12-06 04:52:48 +0000

    CH-Adding participants field

--- coffeetables/models.py
+++ coffeetables/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
+# Snipper taken from ChatGPT
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
 # Create your models here.
 class CoffeeTable(models.Model):
     name = models.CharField(max_length=255)
     image = models.CharField() # TODO add enum for available images or from html select
     description = models.CharField(max_length=255)
+    participants = models.ManyToManyField(User, blank=True)
     # TODO implement other fields
     # createdBy
-    # participants
     # threads
     # time
 
