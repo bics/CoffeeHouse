0458fec6018d00ff9cf7008bfa13bdf8b12e1c38
bics <bicsi94@gmail.com>
2025-12-06 04:42:28 +0000

    CH-Creating table model

--- coffeetables/models.py
+++ coffeetables/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class CoffeeTable(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.CharField() # TODO add enum for available images or from html select
+    description = models.CharField(max_length=255)
+    # TODO implement other fields
+    # createdBy
+    # participants
+    # threads
+    # time
+
+    def __str__(self):
+        return self.name
+
+
+
