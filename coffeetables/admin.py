cd2125afa92d045f6f0c85a0ea06fd473d85a12e
bics <bicsi94@gmail.com>
2025-12-07 20:17:11 +0000

    CH-Adding replies to admin page

--- coffeetables/admin.py
+++ coffeetables/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CoffeeTable
+from .models import CoffeeTable, Reply
 
 # Register your models here.
 # register inspired by tutorial made by Codemy.com
 admin.site.register(CoffeeTable)
+admin.site.register(Reply)
