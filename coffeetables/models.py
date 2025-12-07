f3a937561de0050e63f94270f277fea12819f5f6
bics <bicsi94@gmail.com>
2025-12-07 20:21:12 +0000

    CH-Fixing errors

--- coffeetables/models.py
+++ coffeetables/models.py
@@ -12,13 +12,13 @@ class Reply(models.Model):
     time = models.DateTimeField(editable=False, auto_now_add=True)
     
     def __str__(self):
-        return self.message + ' ' + self.createdBy
+        return self.message + ' ' + self.createdBy.__str__()
     
 
 # model inspired by tutorial made by Codemy.com
 class CoffeeTable(models.Model):
     name = models.CharField(max_length=255)
-    image = models.CharField() # TODO add enum for available images or from html select
+    image = models.CharField(blank=True) # TODO add enum for available images or from html select
     description = models.CharField(max_length=255)
     participants = models.ManyToManyField(User, blank=True)
     replies = models.ManyToManyField(Reply, blank=True)
