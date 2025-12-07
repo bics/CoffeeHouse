b634cb58ac782d496143f6c094221649845ca78c
bics <bicsi94@gmail.com>
2025-12-07 22:35:03 +0000

    CH-Adding selectable img tiles

--- coffeetables/views.py
+++ coffeetables/views.py
@@ -6,5 +6,9 @@ from .models import CoffeeTable
 # view created following tutorial made by Codemy.com
 def tables(request):
     tables_list = CoffeeTable.objects.all()
-    images_list = None
+    images_list = [
+        'static/assets/images/table_images/coffee_drank.jpg',
+        'static/assets/images/table_images/coffee_mug_cartoony.jpg',
+        'static/assets/images/table_images/coffee_mug_tilted.jpg',
+    ]
     return render(request, "tables.html", {'tables_list':tables_list, 'images_list': images_list})
