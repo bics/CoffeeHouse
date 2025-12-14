8238aa5a3b56ed4198395ac0ec9aa2ee8f881619
bics <bicsi94@gmail.com>
2025-12-14 20:07:36 +0000

    CH-Deleting app

--- landingpage/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import redirect
-from django.contrib import messages
-
-# Create your views here.
-# view created following tutorial made by Codemy.com
-def home(request):
-    return redirect("account_login")
