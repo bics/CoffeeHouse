db53548aa8957fd0e0438cd7a63e8c3fe098e2d6
bics <bicsi94@gmail.com>
2025-12-16 04:05:20 +0000

    CH-Adding barebones

--- members/urls.py
+++ members/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [    
     path("register/", RedirectView.as_view(pattern_name="account_signup"), name="register"),
     path("logout_user/", RedirectView.as_view(pattern_name="account_logout"), name="logout_user"),
+    path("account_management/", views.account_management, name="account_management")
 ]
--- members/views.py
+++ members/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
-# view created following tutorial made by Codemy.com https://www.youtube.com/watch?v=HdrOcreAXKk
-def register(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            #login(request, user)
-            #message
-            return redirect('home')
-        else:
-            messages.success(request, ("There were some errors with some fields"))
-            return redirect('register')
-    else:
-        form = RegisterForm()
-    return render(request, "authentication/register.html", { "form": form})
+
+def account_management(request):
+    return render(request, 'account_management.html')
 
 def logout_user(request):
     logout(request)
