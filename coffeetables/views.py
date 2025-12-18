1d40f3865013c35617191492202951bee2290e10
bics <bicsi94@gmail.com>
2025-12-18 03:52:09 +0000

    CH-Adding logic to edit replies

--- coffeetables/views.py
+++ coffeetables/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import CoffeeTable
-from .forms import CoffeeTableForm, ReplyForm
+from .forms import CoffeeTableForm, ReplyForm, UpdateReplyForm
 from django.contrib import messages
 # Snippet taken from ChatGPT
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+
 
 # Create your views here.
 
@@ -36,19 +38,40 @@ def conversation(request, pk):
     table = CoffeeTable.objects.get(pk=pk)
     replies = table.replies.all()
 
+    reply_form = ReplyForm()
+    update_reply_form = UpdateReplyForm()
+
     if request.method == "POST":
-        form = ReplyForm(request.POST)
-        if form.is_valid():
-            obj = form.save(commit=False)
-            obj.createdBy= request.user
-            obj.save()
-            table.replies.add(obj)
-            return redirect('conversation', table.id) 
-        else:            
-            messages.success(request, ("There were some errors with some fields"))
+
+        if "reply-submit" in request.POST:
+            reply_form = ReplyForm(request.POST)
+            if reply_form.is_valid():
+                obj = reply_form.save(commit=False)
+                obj.createdBy= request.user
+                obj.save()
+                table.replies.add(obj)
+                return redirect('conversation', table.id) 
+            else:            
+                messages.success(request, ("There were some errors with some fields"))
+
+        elif "update-reply-submit" in request.POST:
+            reply_id = request.POST.get("reply_id")
+            reply = get_object_or_404(table.replies, pk=reply_id)
+            old_message = reply.message
+            update_reply_form = UpdateReplyForm(request.POST, instance=reply)
+            if update_reply_form.is_valid():
+                new_message = update_reply_form.cleaned_data["message"]
+                reply.history = reply.history +"\n" + old_message
+                reply.edited = True
+                reply.timeEdited = timezone.now()
+                reply.message = new_message
+                reply.save(update_fields=["message", "history", "edited", "timeEdited"])
+                return redirect('conversation', table.id) 
+            else:            
+                messages.success(request, ("There were some errors with some fields"))
     else:
-        form = ReplyForm()
-        return render(request, "conversation.html", {"table": table, "replies": replies, "form": form} )
+        return render(request, "conversation.html", {"table": table, "replies": replies, "reply_form": reply_form, "update_reply_form": update_reply_form})
+    
     
 def close_table(request, pk):    
     table = CoffeeTable.objects.get(pk=pk)
